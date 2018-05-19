b4f4a654d894538642da4971b804aa8684c27f59
Annika Jaakson <jaaksonannika@gmail.com>
2018-05-19 19:23:20 +0300

    Added grid

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include "map.cpp"
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main(int argc, char *argv[]) {
 
    ALLEGRO_DISPLAY *screen = al_create_display(800, 600);
    ALLEGRO_COLOR bg_color = al_map_rgb(50, 50, 50);
+   ALLEGRO_COLOR tile_color = al_map_rgb(100, 100, 100);
    ALLEGRO_EVENT_QUEUE *events = al_create_event_queue();
    ALLEGRO_EVENT next_event;
 
@@ -22,9 +24,11 @@ int main(int argc, char *argv[]) {
    al_register_event_source(events, al_get_keyboard_event_source());
    al_register_event_source(events, al_get_mouse_event_source());
 
-   while (1) {
-      al_clear_to_color(bg_color);
+   Map tilemap = Map(800, 600);
 
+   al_clear_to_color(bg_color);
+
+   while (1) {
       al_get_next_event(events, &next_event);
 
       if (next_event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -32,6 +36,8 @@ int main(int argc, char *argv[]) {
          al_destroy_event_queue(events);
          break;
       }
+
+      tilemap.draw(tile_color);
 
       al_flip_display();
       al_rest(0.01f);
---
+++ map.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+
+using namespace std;
+
+class Map {
+public:
+   int tile_size;
+   int tile_distance;
+   int map_width;
+   int map_height;
+   int map_grid[10][10];
+   int map_start_x;
+   int map_start_y;
+
+   Map(int screen_width, int screen_height) {
+      tile_size = 40;
+      tile_distance = 5;
+      map_width = 10;
+      map_height = 10;
+
+      map_start_x = (screen_width - map_width*tile_size - (map_width-1)*tile_distance)/2;
+      map_start_y = (screen_height - map_height*tile_size - (map_height-1)*tile_distance)/2;
+   }
+
+   void draw(ALLEGRO_COLOR tile_color) {
+      int starting_x;
+      int starting_y;
+
+      for (int i = 0; i < map_width; i++) {
+         for (int j = 0; j < map_height; j++) {
+            cout << i*j << endl;
+            starting_x = map_start_x+i*tile_size+(i-1)*tile_distance;
+            starting_y = map_start_y+j*tile_size+(j-1)*tile_distance;
+            al_draw_filled_rectangle(starting_x, starting_y, starting_x + tile_size, starting_y + tile_size, tile_color);
+         }
+      }
+   }
+};
